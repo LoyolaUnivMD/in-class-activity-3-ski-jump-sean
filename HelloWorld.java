72b086d127162ae79c62ea161ea98b9841449c93
seantriplett <srtriplett@loyola.edu>
2024-02-01 14:40:45 -0500

    Edited code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,20 @@
-//HelloWorld.java
 
+import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the type of ski jump (normal or large):");
+        String jumpType = input.nextLine().trim().toLowerCase();
+        while(!jumpType.equals("normal") && !jumpType.equals("large")) {
+            System.out.println("Invalid. Please enter the type of ski jump (normal or large):");
+            jumpType = input.nextLine().trim().toLowerCase();
+        }
+        if (jumpType.equals("normal")) {
+            int height = 46;
+            double pointsPerMeter = 2;
+            int par = 90;
+        }
         
     }
 }
